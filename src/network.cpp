9222d34f6a2dcb5f57ae85772fdd2c80349d490b
Leonard Karsunky <leonardkarsunky@tsf-492-wpa-4-157.epfl.ch>
2018-11-05 14:58:19 +0100

    Added all files

---
+++ src/network.cpp
@@ -0,0 +1,98 @@
+#include "network.h"
+#include "random.h"
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+
+void Network::resize(const size_t& n) {
+	values.resize(n);
+	if (n < values.size()) {
+		for (size_t i(n); i < values.size(); ++i) {
+			std::multimap<size_t, size_t>::iterator it = links.find(i);
+			links.erase(it);
+		}
+	}
+	RandomNumbers tmp;
+	for (size_t i(0); i < values.size(); ++i) {
+		values[i] = tmp.normal(0,1);
+	}
+}
+
+bool Network::add_link(const size_t& a, const size_t& b) {
+	if ((a < values.size()) and (b < values.size()) and (a != b)) {
+		auto result = links.equal_range(a);
+		for (auto it(result.first); it != result.second; it++) {
+			if (it->second == b) {
+				return false;
+			}
+		}
+		links.insert(std::pair<size_t, size_t>(a, b));
+		links.insert(std::pair<size_t, size_t>(b, a));
+		return true;
+	}
+	return false;
+}
+
+size_t Network::random_connect(const double& mean_deg) {
+	links.clear();
+	RandomNumbers degrees;
+	
+	for (size_t n(0); n < values.size(); ++n) {
+		size_t tmp(degrees.poisson(mean_deg));
+		for (size_t i(0); i < tmp; ++i) {
+			add_link(n, degrees.uniform_double(0, values.size()-1));
+		}
+	}
+	return links.size()/2;
+}
+
+size_t Network::set_values(const std::vector<double>& new_values) {
+	size_t old_size(values.size());
+	size_t new_size(new_values.size());
+	
+	if (new_size <= old_size) {
+		for (size_t i(0); i < new_size; ++i) {
+			values[i] = new_values[i];
+		}
+		return new_size;
+	} else {
+		for (size_t i(0); i < old_size; ++i) {
+			values[i] = new_values[i];
+		}
+		return old_size;
+	}
+}
+
+size_t Network::size() const {
+	return values.size();
+}
+
+size_t Network::degree(const size_t &n) const {
+	size_t nb_degrees(0);
+	auto result = links.equal_range(n);
+	for (auto it(result.first); it != result.second; it++) {
+		++nb_degrees;
+	}
+	return nb_degrees;
+}
+
+double Network::value(const size_t &n) const {
+	return values[n];
+}
+
+std::vector<double> Network::sorted_values() const {
+	std::vector<double> tmp = values;
+	std::sort (tmp.begin(), tmp.end());
+	std::reverse (tmp.begin(), tmp.end());
+	return tmp;
+}
+
+std::vector<size_t> Network::neighbors(const size_t& n) const {
+	std::vector<size_t> tmp;
+	auto result = links.equal_range(n);
+		for (auto it(result.first); it != result.second; it++) {
+			tmp.push_back(it->second);
+		}
+	return tmp;
+}
+	
